2a2b3958cc19241248156eac3d117bdaa962a36c
SttackSite <sttacksite@gmail.com>
2026-03-09 17:31:08 -0300

    Update template29.py

--- templates/template29.py
+++ templates/template29.py
@@ -7,7 +7,7 @@ from email.mime.text import MIMEText
 # ─────────────────────────────────────────────────────────────────────────────
 # CONFIGURAÇÕES FIXAS
 # ─────────────────────────────────────────────────────────────────────────────
-TEMPLATE_IMAGE_URL = "https://raw.githubusercontent.com/SttackSite/templatestestes/main/img29.png"
+TEMPLATE_IMAGE_URL = "https://raw.githubusercontent.com/SttackSite/editor/main/img29.png"
 TEMPLATE_NAME      = "Template 29 — Website Sustentável Style (Eco-Design & Tech)"
 TEMPLATE_ID        = "template_29"
 GMAIL_USER         = st.secrets.get("GMAIL_USER", "")
