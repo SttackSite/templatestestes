bf5befc4b6fe5d156009a0c5004d0565fbbbaa5d
SttackSite <sttacksite@gmail.com>
2026-03-09 17:31:31 -0300

    Update template31.py

--- templates/template31.py
+++ templates/template31.py
@@ -7,7 +7,7 @@ from email.mime.text import MIMEText
 # ─────────────────────────────────────────────────────────────────────────────
 # CONFIGURAÇÕES FIXAS
 # ─────────────────────────────────────────────────────────────────────────────
-TEMPLATE_IMAGE_URL = "https://raw.githubusercontent.com/SttackSite/templatestestes/main/img31.png"
+TEMPLATE_IMAGE_URL = "https://raw.githubusercontent.com/SttackSite/editor/main/img31.png"
 TEMPLATE_NAME      = "Template 31 — Epiminds Style (Neurotech & AI)"
 TEMPLATE_ID        = "template_31"
 GMAIL_USER         = st.secrets.get("GMAIL_USER", "")
