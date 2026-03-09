932fc190390f9b0477dacf9a9d663d35d4d890da
SttackSite <sttacksite@gmail.com>
2026-03-09 17:26:29 -0300

    Update template6.py

--- templates/template6.py
+++ templates/template6.py
@@ -7,7 +7,7 @@ from email.mime.text import MIMEText
 # ─────────────────────────────────────────────────────────────────────────────
 # CONFIGURAÇÕES FIXAS
 # ─────────────────────────────────────────────────────────────────────────────
-TEMPLATE_IMAGE_URL = "https://raw.githubusercontent.com/SttackSite/templatestestes/main/img6.png"
+TEMPLATE_IMAGE_URL = "https://raw.githubusercontent.com/SttackSite/editor/main/img6.png"
 TEMPLATE_NAME      = "Template 6 — Alta Precisão (Bautz Style)"
 TEMPLATE_ID        = "template_6"
 GMAIL_USER         = st.secrets.get("GMAIL_USER", "")
