08f1ab47f4d5507adf6bed51e0d44748edb0c0a4
SttackSite <sttacksite@gmail.com>
2026-03-09 17:27:55 -0300

    Update template13.py

--- templates/template13.py
+++ templates/template13.py
@@ -7,7 +7,7 @@ from email.mime.text import MIMEText
 # ─────────────────────────────────────────────────────────────────────────────
 # CONFIGURAÇÕES FIXAS
 # ─────────────────────────────────────────────────────────────────────────────
-TEMPLATE_IMAGE_URL = "https://raw.githubusercontent.com/SttackSite/templatestestes/main/img13.png"
+TEMPLATE_IMAGE_URL = "https://raw.githubusercontent.com/SttackSite/editor/main/img13.png"
 TEMPLATE_NAME      = "Template 13 — Ogreen Style (Sustentabilidade & Indústria)"
 TEMPLATE_ID        = "template_13"
 GMAIL_USER         = st.secrets.get("GMAIL_USER", "")
